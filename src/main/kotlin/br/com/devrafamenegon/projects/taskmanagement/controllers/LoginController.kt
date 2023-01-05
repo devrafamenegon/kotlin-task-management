6ac32ef5710f9aa9c7d3ed86e174eab9b6d01e17
Rafael Menegon <dev.rafaelmenegon@gmail.com>
2023-01-04 22:52:12 -0300

    feat: simple tratative login

--- src/main/kotlin/br/com/devrafamenegon/projects/taskmanagement/controllers/LoginController.kt
+++ src/main/kotlin/br/com/devrafamenegon/projects/taskmanagement/controllers/LoginController.kt
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import java.lang.RuntimeException
 
 @RestController
 @RequestMapping("api/login")
@@ -16,7 +15,15 @@ class LoginController {
     @PostMapping
     fun login(@RequestBody dto : LoginDTO) : ResponseEntity<Any> {
         try {
-            throw RuntimeException("Testing Exception")
+            if (dto.login.isBlank() || dto.login.isEmpty() || dto.password.isBlank() || dto.password.isEmpty()) {
+                return ResponseEntity(
+                    ErrorDTO(HttpStatus.BAD_REQUEST.value(), "Invalid parameters"),
+                    HttpStatus.BAD_REQUEST
+                )
+            }
+
+            return ResponseEntity("Login successfully", HttpStatus.OK)
+
         } catch (exception: Exception) {
             return ResponseEntity(
                 ErrorDTO(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Failed to login, try again"),
