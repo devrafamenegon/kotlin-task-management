4d073bd2dfae91dbb4534661560f603dc8427f7d
Rafael Menegon <dev.rafaelmenegon@gmail.com>
2023-01-06 15:47:02 -0300

    feat: add cors config

--- src/main/kotlin/br/com/devrafamenegon/projects/taskmanagement/configurations/SecurityConfiguration.kt
+++ src/main/kotlin/br/com/devrafamenegon/projects/taskmanagement/configurations/SecurityConfiguration.kt
@@ -4,12 +4,16 @@ import br.com.devrafamenegon.projects.taskmanagement.filters.JWTAuthorizerFilter
 import br.com.devrafamenegon.projects.taskmanagement.repositories.UserRepository
 import br.com.devrafamenegon.projects.taskmanagement.utils.JWTUtils
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @Configuration
 @EnableWebSecurity
@@ -30,12 +34,27 @@ class SecurityConfiguration : WebSecurityConfigurerAdapter() {
                 .antMatchers(HttpMethod.POST, "/api/user").permitAll()
                 .anyRequest().authenticated()
 
+            // cors configuration public
+            http.cors().configurationSource(configureCors())
+
             // security filter
             http.addFilter(JWTAuthorizerFilter(authenticationManager(), jwtUtils, userRepository))
 
             // storage session with tokens
             http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
         }
+    }
 
+    @Bean
+    fun configureCors() : CorsConfigurationSource {
+        val configuration = CorsConfiguration()
+
+        configuration.allowedOrigins = mutableListOf("*")
+        configuration.allowedMethods = mutableListOf("*")
+        configuration.allowedHeaders = mutableListOf("*")
+
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", configuration)
+        return source
     }
 }
